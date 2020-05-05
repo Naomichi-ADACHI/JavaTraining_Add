507086445057d7cda773d0f065020ba16f77e262
Naomichi Adachi <naoyk1212@yahoo.co.jp>
2020-05-05 20:08:35 +0900

    追加総合演習2完了まで

---
+++ src/ex/Account.java
@@ -0,0 +1,38 @@
+/**
+ * アカウント情報クラス
+ */
+public class Account {
+    private String id;  // ログイン用id
+    private String password;    // ログイン用password
+
+    /**
+     * コンストラクタ
+     * ※引数なしは許さない
+     * メンバ変数に値をセットする
+     *
+     * @param id       ログインid
+     * @param password ログインpassword
+     */
+    public Account(final String id, final String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    /**
+     * メンバ変数idの値を返す
+     *
+     * @return idの値
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * メンバ変数passwordの値を返す
+     *
+     * @return passwordの値
+     */
+    public String getPassword() {
+        return password;
+    }
+}
